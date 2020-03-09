d873abf350dc926bf1a6e57a82f13b1e962504cf
a01375332 <leonardocastillejos03@gmail.com>
2020-03-09 13:23:22 -0600

    Initial commit

---
+++ app/src/main/java/mx/itesm/rockpaperscissor/AcercaDe.kt
@@ -0,0 +1,12 @@
+package mx.itesm.rockpaperscissor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AcercaDe : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_acerca_de)
+    }
+}
